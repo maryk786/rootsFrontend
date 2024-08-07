ef43c82bd9c8058928d9f45a0afb3dde7ea10962
maryk786 <mariamzarnab104@gmail.com>
2024-08-07 11:29:53 +0500

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Slider from "../components/Slider"
+import TopCategories from '../components/TopCategory'
+import CallSection from "../components/CallSection"
+import FeaturedProduct from '../components/FeaturedProduct'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Slider/>
+      <TopCategories/>
+      <FeaturedProduct/>
+      <CallSection/>
+    </div>
+  )
+}
+
+export default Home
